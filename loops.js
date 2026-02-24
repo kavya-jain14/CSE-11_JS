cd86330b6a5139263dcf9ba6ef418c4ecdff9ee7
Kavya <kavyajain1407@gmail.com>
2026-02-24 10:28:44 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,11 @@
+let i;
+for(i = 0 ; i<3 ; i = i + 1){
+    console.log(i);
+}
+let count;
+document.write("Starting Loop" + "<br/>");
+for(i = 0 ; i < 10 ; i++){
+    document.write("Current count = " + i);
+    document.write("<br/>");
+}
+document.write("Loop stopped!");
