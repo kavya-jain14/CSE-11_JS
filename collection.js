32f96c21f8c94370a2a927d0beac6f7ac6b3b87c
Kavya <kavyajain1407@gmail.com>
2026-03-10 09:38:39 +0530

    collection

---
+++ collection.js
@@ -0,0 +1,17 @@
+let collection = [
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    90009898193,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+
+];
+console.log(collection);
+collection.teacherName="john mac";
+collection.phoneNo="950008193";
+console.log(collection);
