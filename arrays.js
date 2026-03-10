646680726dff8bc39b4502cc42d7309552b05152
Kavya <kavyajain1407@gmail.com>
2026-03-10 09:03:21 +0530

    Arrays

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let animals= new Array("cat", "dog", "lion");
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1==sym2)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
