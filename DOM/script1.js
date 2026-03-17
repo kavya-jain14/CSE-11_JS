0064cb8e110862464073450bb0b7622ff5536a0c
Kavya <kavyajain1407@gmail.com>
2026-03-17 10:20:08 +0530

    Script of list

--- DOM/script1.js
+++ DOM/script1.js
@@ -1,35 +1,35 @@
-/*const result = document.getElementById("para");
+const result = document.getElementById("para");
 result.innerHTML = "Hello, World!";
  
-//const paragraphs = document.getElementsByClassName("intro");
-//paragraphs[0].innerHTML = "This is the first paragraph.";
-//paragraphs[1].innerHTML = "This is the second paragraph.";
-//paragraphs[2].innerHTML = "This is the third paragraph.";
+const paragraphs = document.getElementsByClassName("intro");
+paragraphs[0].innerHTML = "This is the first paragraph.";
+paragraphs[1].innerHTML = "This is the second paragraph.";
+paragraphs[2].innerHTML = "This is the third paragraph.";
 
 
-/*const abc = document.getElementsByTagName("p");
+const abc = document.getElementsByTagName("p");
 abc[0].innerHTML = "This is the not first paragraph.";
 abc[2].style.color = "red";
 abc[1].style.fontSize = "20px";
 abc[1].style.fontFamily = " Arial, sans-serif";
-abc[0].style.backgroundColor = "yellow";*/
+abc[0].style.backgroundColor = "yellow";
 
-/*const test = document.querySelectorAll(".intro");
+const test = document.querySelectorAll(".intro");
 for(element in test)  {
   element.style.color = "blue";
   element.style.fontSize = "18px";
   element.style.fontFamily = "Arial, sans-serif";
   element.style.backgroundColor = "lightgray";
-};*/
-/*const result2 = document.getElementById("para");
-result2.innerHTML = "This is the new content of the div element.";*/
+};
+const result2 = document.getElementById("para");
+result2.innerHTML = "This is the new content of the div element.";
 
-/*const newElement = document.querySelectorAll("ul li");
+const newElement = document.querySelectorAll("ul li");
 for(x in newElement) {
   newElement[x].style.color = "green";
   newElement[x].style.fontSize = "16px";
   newElement[x].style.fontFamily = "Arial, sans-serif";
-}*/
+}
 function changeContent() {
   document.getElementById("heading").setAttribute("class", "new-heading");
   alert("Content changed!");
